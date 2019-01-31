b9e61b2d67fd4b848ed497674477610491655d00
Ben Cutter <35261404+Cutter1998@users.noreply.github.com>
2019-01-31 23:47:51 +0000

    Added carousel to index

---
+++ js/Carousel.js
@@ -0,0 +1,42 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//buttons
+const prevBtn = document.querySelector('#prevButton');
+const nextBtn = document.querySelector('#nextButton');
+
+//counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+
+nextButton.addEventListener('click',()=>{
+	if (counter >= carouselImages.length - 1) return;
+	carouselSlide.style.transition = "transform 0.4s ease-in-out";
+	counter++;
+	carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+	console.log(counter);
+});
+
+prevButton.addEventListener('click',()=>{
+	if (counter <= 0) return;
+	carouselSlide.style.transition = "transform 0.4s ease-in-out";
+	counter--;
+	carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+	console.log(counter);
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+	if(carouselImages[counter].id === 'lastClone'){
+		carouselSlide.style.transition = "none";
+		counter = carouselImages.length - 2;
+		carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+	}
+	
+	if(carouselImages[counter].id === 'firstClone'){
+		carouselSlide.style.transition = "none";
+		counter = carouselImages.length - counter;
+		carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+	}
+});
