4d69615eec38acdd2e7dfb19e00e26a289642e04
Ben Cutter <35261404+Cutter1998@users.noreply.github.com>
2019-03-05 22:43:56 +0000

    Final contact form

--- js/ColourPicker.js
+++ js/ColourPicker.js
@@ -2,14 +2,17 @@ document.addEventListener("DOMContentLoaded", function(){
 	if(localStorage.getItem("bannerColour") === null){
 		document.getElementsByClassName('nav')[0].style.backgroundColor = "#60bdb1"
 		document.getElementById('colourPicker').value = "#60bdb1";
+		document.getElementById('contactMeContainer').style.backgroundColor = "#60bdb1";
 	}
 	else{
 		document.getElementById('colourPicker').value = localStorage.getItem("bannerColour");
 		document.getElementsByClassName('nav')[0].style.backgroundColor = localStorage.getItem("bannerColour");
+		document.getElementById('contactMeContainer').style.backgroundColor = localStorage.getItem("bannerColour");
 	}
 })
 
 function changeBannerColour(){
 	document.getElementsByClassName('nav')[0].style.backgroundColor = document.getElementById('colourPicker').value;
+	document.getElementById('contactMeContainer').style.backgroundColor = document.getElementById('colourPicker').value;
 	localStorage.setItem("bannerColour", document.getElementById('colourPicker').value);
 }
